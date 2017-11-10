7facf3ef92dc9f745a16cb164b9ef8de19c4a5a3
Joe Kim <josephkim0224@gmail.com>
2017-11-10 06:23:15 -0500

    send email on form submit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var helper = require('sendgrid').mail;
+var from_email = new helper.Email('josephkim0224@gmail.com');
+var to_email = new helper.Email('josephkim0224@gmail.com');
+var subject = 'Hello World from the SendGrid Node.js Library!';
+var content = new helper.Content('text/plain', 'Hello, Email!');
+var mail = new helper.Mail(from_email, subject, to_email, content);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -29,6 +36,36 @@ router.get('/project/:name', function(req, res, next) {
 router.post('/:action', function(req, res, next) {
   var action = req.params.action
   if (action == 'contact') {
+
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON(),
+    });
+
+    sg.API(request, function(error, response) {
+      console.log(response.statusCode);
+      console.log(response.body);
+      console.log(response.headers);
+    
+      if (error) {
+        res.json({
+          confirmation: 'fail',
+          message: error
+        })
+
+        return
+      }
+
+      res.json({
+        confirmation: 'success',
+        response: response.body
+      })
+
+      return
+    });
+    
     res.redirect('/confirmation')  
   }
 })
