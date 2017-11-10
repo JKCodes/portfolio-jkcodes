63b34006f894958438a4fb598c7b01a245693577
Joe Kim <josephkim0224@gmail.com>
2017-11-10 08:33:07 -0500

    configure gzip compression

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var sendgrid = require('./routes/sendgrid');
+var compression = require('compression')
 require('dotenv').config()
 
 var app = express();
@@ -23,6 +24,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
