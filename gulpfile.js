1a0217e4d84f84898b09f2567afaad6dc743ba62
Joe Kim <josephkim0224@gmail.com>
2017-11-10 07:46:09 -0500

    create style.min.css and use that instead of making 10+ css requests

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gp_concat = require('gulp-concat')
 var gp_rename = require('gulp-rename')
 var gp_uglify = require('gulp-uglify')
 var minifyCSS = require('gulp-minify-css')
+var autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('build', function() {
   return gulp.src(
@@ -19,4 +20,23 @@ gulp.task('build', function() {
     .pipe(gulp.dest('./public/build/'));
 })
 
+gulp.task('css', function() {
+  return gulp.src(
+      [
+        './public/css/bootstrap.css',
+        './public/style.css',
+        './public/css/swiper.css',
+        './public/css/dark.css',
+        './public/css/font-icons.css',
+        './public/css/animate.css',
+        './public/css/magnific-popup.css',
+        './public/css/responsive.css',
+        './public/css/custom.css',        
+      ]
+    )
+    .pipe(minifyCSS())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+    .pipe(gp_concat('style.min.css'))
+    .pipe(gulp.dest('./public/build/css/'))
+})
 gulp.task('default', ['build'], function(){})
