88197c0b659db8590456ddf851d944f1b0c747ef
Joe Kim <josephkim0224@gmail.com>
2017-11-11 07:58:30 -0500

    create Project schema

---
+++ models/Project.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose')
+
+var ProjectSchema = new mongoose.Schema({
+  name: {type:String, default: ''},
+  description: {type:String, default: ''},
+  image: {type:String, default: ''},
+  tools: {type:Array, default:[]},
+  timestamp: {type:Date, default: Date.now}
+})
+
+module.exports = mongoose.model('ProjectSchema', ProjectSchema)
