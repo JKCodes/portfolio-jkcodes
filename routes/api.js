2ae460cdadc64aac22e103798cde484a04b116ac
Joe Kim <josephkim0224@gmail.com>
2017-11-13 09:02:09 -0500

    add get and getById api routes

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,50 @@
 var express = require('express')
 var router = express.Router()
+var Project = require('../models/Project')
 
-router.get('/', function(req, res, next) {
-  res.json({
-    confirmation: 'success',
-    message: 'TEST'
-  })
+router.get('/:resource', function(req, res, next) {
+  var resource = req.params.resource
+
+  if (resource == 'project') {
+    Project.find(null, function(err, projects) {
+      if (err) {
+        res.json({
+          confirmation: 'fail',
+          message: err
+        })
+
+        return
+      }
+
+      res.json({
+        confirmation: 'success',
+        results: projects
+      })
+    })
+  }
+})
+
+router.get('/:resource/:id', function(req, res, next) {
+  var resource = req.params.resource
+  var id = req.params.id
+
+  if (resource == 'project') {
+    Project.findById(id, function(err, project) {
+      if (err) {
+        res.json({
+          confirmation: 'fail',
+          message: err
+        })
+
+        return
+      }
+
+      res.json({
+        confirmation: 'success',
+        result: project
+      })
+    })
+  }
 })
 
 module.exports = router
