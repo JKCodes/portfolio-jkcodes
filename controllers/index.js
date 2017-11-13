54db072eeca47fe7d4b72ec26d6affff1b01c830
Joe Kim <josephkim0224@gmail.com>
2017-11-13 09:43:52 -0500

    completely remove all controller and model related code from api route

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+var ProjectController = require('./ProjectController')
+var InquiryController = require('./InquiryController')
+
+module.exports = {
+  project: ProjectController,
+  inquiry: InquiryController
+}
