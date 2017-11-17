6a4313237f39d5000bc41d5ce321498c1770f22a
Joe Kim <josephkim0224@gmail.com>
2017-11-17 14:06:10 -0500

    implement method-override on app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var index = require('./routes/index');
 var api = require('./routes/api')
 var compression = require('compression')
@@ -32,6 +33,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
